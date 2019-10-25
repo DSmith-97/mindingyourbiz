ddd0fb70c81acb8cdde24d1e1b40c5c892d729d1
Derrick Smith <prof_derricksmith@yahoo.com>
2019-10-24 23:40:25 -0400

    moves scripts to file add json file

---
+++ scripts/json.js
@@ -0,0 +1,13 @@
+var dataStore = {
+  "subscriberIndex": "0",
+  "contactIndex" : "0",
+  "subscriberList" : {
+    "email_0" : "prof_derricksmith@yhoo.com",
+    "email_1" : "babypiety@yahoo.com"
+  },
+  "contactList" : {
+    "name" : "Derrick Smith",
+    "email" : "testEmail1@email.com",
+    "message" : "This is the first message"
+  }
+}
---
+++ scripts/site.js
@@ -0,0 +1,57 @@
+// Script to open and close sidebar when on tablets and phones
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
+}
+
+function showName(x) {
+  var newSrc = x.children[0].src;
+  $("#sample").modal("show");
+  var newImage = "<img src='./images/markhotwaterpressure/mark_sample1.png' >";
+  var nImg = "<img src='" + newSrc + "' >";
+  $(".modal-body").empty();
+  $(".modal-body").append(nImg);
+  // $(".main_image").attr("src", newSrc);
+  // imgElement.setAttribute("src", "ff")
+  console.log("imgElement : " + newSrc);
+}
+
+// Slideshow Apartment Images
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-opacity-off1", "");
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " w3-opacity-off1";
+}
+
+function getSubscriberEmail() {
+  var subscriberEmail;
+  $.ajax({
+    url:
+  });
+}
