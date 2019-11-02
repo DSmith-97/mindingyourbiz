7cb8271ba9570efcb93e04564f6414ad87ae9edf
Derrick Smith <prof_derricksmith@yahoo.com>
2019-11-02 12:31:28 -0400

    save JSON file

--- scripts/json.js
+++ scripts/json.js
@@ -11,5 +11,31 @@ var dataStore = {
       "email" : "testEmail1@email.com",
       "message" : "This is the first message"
     }
+  ],
+  "newBusinessList" : [
+    {
+      "ownerName" : "Mr. Pibb",
+      "contactNumber" : "555-222-2345",
+      "businessName" : "business1",
+      "type" : "pest control",
+      "city" : "palm bay",
+      "state" : "Florida",
+      "contactable" : "yes",
+      "comment" : "some comment here",
+      "active" : "yes"
+    }
   ]
 }
+
+
+// save data store object to myjson
+$.ajax({
+    url:"https://api.myjson.com/bins/7i1p8",
+    type:"PUT",
+    data: JSON.stringify(dataStore),
+    contentType:"application/json; charset=utf-8",
+    dataType:"json",
+    success: function(data, textStatus, jqXHR){
+		console.log(textStatus);
+    }
+});   
